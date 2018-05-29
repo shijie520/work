9d698099173939de5b4d1d1c7f7266482bd14e60
jay <you@example.com>
2018-05-29 16:19:18 +0800

    5month 29day 16:20time

--- blog/index/urls.py
+++ blog/index/urls.py
@@ -4,7 +4,8 @@ from . import views as app
 urlpatterns = [
     url(r'^home$',app.date, name ='date'),
     url(r'^date/(?P<date_page_id>[0-9]+)/$',app.date_page, name='date_page'),
-    url(r'^edit/$',app.edit_page, name ='edit_page'),
+    url(r'^edit/(?P<edit_page_id>[0-9]+)/$',app.edit_page, name ='edit_page'),
     url(r'^post/$',app.page_post, name ='page_post'),
+    url(r'^home$',app.return_home, name ='retuen_home'),
 
 ]
--- blog/index/views.py
+++ blog/index/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 # Create your views here.创建应用
 from . import models
 
+
+def return_home(request):
+	#acts = models.date.objects.all() #获取数据库，返回一张表单 
+	return render(request,'homepage.html')#返回一个页面，链接acts 键值获取里面表单
+
+
+
 def date(request):  #请求
 	acts = models.date.objects.all() #获取数据库，返回一张表单 
 	return render(request,'homepage.html',{'acts': acts})#返回一个页面，链接acts 键值获取里面表单
@@ -13,12 +20,12 @@ def date_page(request,date_page_id):
 	return render(request,'date_page.html',{'act': act})
 
 
-def edit_page(request,edit_page_id= 0):
+def edit_page(request,edit_page_id):
 	if str(edit_page_id) =='0':
 		return render(request,'edit_page.html')
 	else:
-		act = models.data.objects.get(pk= edit_page_id)
-		return render(request,'edit_page.html',{'act': act})
+		acts = models.date.objects.get(pk = edit_page_id)
+		return render(request,'edit_page.html',{'acts':acts})
 
 
 
@@ -26,7 +33,7 @@ def edit_page(request,edit_page_id= 0):
 def page_post(request):
 	title = request.POST.get('title', 'name')#获取前端数据
 	content = request.POST.get('content','content')
-
+	id = request.POST.get('id','0')
 	olds = models.date.objects.all()#获取全部数据	
 	if title == 'name' :#不能让用户使用默认值
 		return HttpResponse('Input of The Error')
@@ -34,11 +41,19 @@ def page_post(request):
 		return HttpResponse('Can"t be empty')
 	elif content == '':#判断内容是否为空
 		return HttpResponse('Can"t be empty')
-	for old in olds:#数据库遍历
-		if title == old.title:#判断是否重复提交
-			return HttpResponse(' already The title submitted it')
-	else:#一切OK准备创建
+	else:	
+		for old in olds:#数据库遍历
+			if content == old.content:#判断是否重复提交
+				return HttpResponse(' already The content submitted it')
+	
+	if id == '0':
 		data = models.date.objects.create(title=title,content=content)
 		acts = models.date.objects.all()#创建成功重新刷新页面获取数据
 		return render(request,'homepage.html',{'acts': acts})
 
+	else:
+		acts = models.date.objects.get(pk = id)
+		acts.title = title
+		acts.content = content
+		acts.save()
+		return render(request,'date_page.html',{'act': acts})
