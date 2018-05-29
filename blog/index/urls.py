9309c977b30f77d9adf71bcff21fe206e1e1e45d
jay <you@example.com>
2018-05-29 13:06:19 +0800

    the is blog of django-python

--- blog/index/urls.py
+++ blog/index/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url,include
 from . import views as app
 
 urlpatterns = [
-    url(r'^$',app.date, name ='index'),
+    url(r'^home$',app.date, name ='date'),
     url(r'^date/(?P<date_page_id>[0-9]+)/$',app.date_page, name='date_page'),
-    
+    url(r'^edit/$',app.edit_page, name ='edit_page'),
+    url(r'^post/$',app.page_post, name ='page_post'),
 
 ]
--- blog/index/views.py
+++ blog/index/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
 from . import models
 def date(request):
@@ -8,6 +9,28 @@ def date(request):
 
 def date_page(request,date_page_id):
 	act = models.date.objects.get(pk = date_page_id)
-	return render(request,'date_page.html',{'act':act})
+	return render(request,'date_page.html',{'act': act})
 
+
+def edit_page(request):
+	return render(request,'edit_page.html')
+
+
+def page_post(request):
+	title = request.POST.get('title', 'name')
+	content = request.POST.get('content','content')
+	olds = models.date.objects.all()	
+	if title == 'name' :
+		return HttpResponse('Input of The Error')
+	elif title == '' and content == '':
+		return HttpResponse('Can"t be empty')
+	elif content == '':
+		return HttpResponse('Can"t be empty')
+	for old in olds:
+		if title == old.title:
+			return HttpResponse(' already The title submitted it')
+	else:
+		data = models.date.objects.create(title=title,content=content)
+		acts = models.date.objects.all()
+		return render(request,'homepage.html',{'acts': acts})
 
